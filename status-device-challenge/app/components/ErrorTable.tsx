55a99e10b7fa46043ba1a24225018787198f99ad
Luis Defago <102487252+luisdefago@users.noreply.github.com>
2024-06-04 10:25:07 -0300

    feat: Empty Devices Error

---
+++ status-device-challenge/app/components/ErrorTable.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { TbFaceIdError } from "react-icons/tb";
+
+const ErrorTable = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-80 text-gray-200">
+      <TbFaceIdError size={140} className="mb-2" />
+      <p className="text-2xl">No se encontraron dispositivos.</p>
+    </div>
+  );
+};
+
+export default ErrorTable;
--- status-device-challenge/app/components/Table.tsx
+++ status-device-challenge/app/components/Table.tsx
@@ -5,6 +5,7 @@ import getDevices from '@/API/devicesData';
 import { Device } from '@/interfaces/interface';
 import { useDispatch, useSelector } from 'react-redux';
 import ButtonPaged from './ButtonPaged';
+import ErrorTable from './ErrorTable';
 
 const Table = () => {
   const dispatch = useDispatch();
@@ -30,35 +31,41 @@ const Table = () => {
 
   return (
     <div className="overflow-x-auto p-4 bg-gray-900 rounded-lg shadow-lg">
-      <table className="min-w-full bg-gray-800 text-gray-200 rounded-lg overflow-hidden">
-        <thead>
-          <tr>
-            <th className="py-2 px-4 border-b border-gray-700 text-center">ID</th>
-            <th className="py-2 px-4 border-b border-gray-700 text-center">Nombre</th>
-            <th className="py-2 px-4 border-b border-gray-700 text-center">Batería</th>
-            <th className="py-2 px-4 border-b border-gray-700 text-center">Estado</th>
-            <th className="py-2 px-4 border-b border-gray-700 text-center">SOS</th>
-            <th className="py-2 px-4 border-b border-gray-700 text-center">WiFi</th>
-            <th className="py-2 px-4 border-b border-gray-700 text-center">Propietario</th>
-            <th className="py-2 px-4 border-b border-gray-700 text-center">Contactos</th>
-          </tr>
-        </thead>
-        <tbody>
-          {currentDevices.map((device) => (
-            <tr key={device.id} className="hover:bg-gray-700">
-              <td className="py-2 px-4 border-b border-gray-700 text-center">{device.id}</td>
-              <td className="py-2 px-4 border-b border-gray-700 text-center">{device.name}</td>
-              <td className="py-2 px-4 border-b border-gray-700 text-center">{device.battery}%</td>
-              <td className="py-2 px-4 border-b border-gray-700 text-center">{device.connected ? 'Conectado' : 'Desconectado'}</td>
-              <td className="py-2 px-4 border-b border-gray-700 text-center">{device.isSos ? 'Activado' : 'Desactivado'}</td>
-              <td className="py-2 px-4 border-b border-gray-700 text-center">{device.isWifi ? 'Sí' : 'No'}</td>
-              <td className="py-2 px-4 border-b border-gray-700 text-center">{device.owner}</td>
-              <td className="py-2 px-4 border-b border-gray-700 text-center">{device.contacts.join(', ')}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <ButtonPaged totalDevices={filteredDevices.length} devicesPerPage={devicesPerPage} />
+      {currentDevices.length === 0 ? (
+        <ErrorTable />
+      ) : (
+        <>
+          <table className="min-w-full bg-gray-800 text-gray-200 rounded-lg overflow-hidden">
+            <thead>
+              <tr>
+                <th className="py-2 px-4 border-b border-gray-700 text-center">ID</th>
+                <th className="py-2 px-4 border-b border-gray-700 text-center">Nombre</th>
+                <th className="py-2 px-4 border-b border-gray-700 text-center">Batería</th>
+                <th className="py-2 px-4 border-b border-gray-700 text-center">Estado</th>
+                <th className="py-2 px-4 border-b border-gray-700 text-center">SOS</th>
+                <th className="py-2 px-4 border-b border-gray-700 text-center">WiFi</th>
+                <th className="py-2 px-4 border-b border-gray-700 text-center">Propietario</th>
+                <th className="py-2 px-4 border-b border-gray-700 text-center">Contactos</th>
+              </tr>
+            </thead>
+            <tbody>
+              {currentDevices.map((device) => (
+                <tr key={device.id} className="hover:bg-gray-700">
+                  <td className="py-2 px-4 border-b border-gray-700 text-center">{device.id}</td>
+                  <td className="py-2 px-4 border-b border-gray-700 text-center">{device.name}</td>
+                  <td className="py-2 px-4 border-b border-gray-700 text-center">{device.battery}%</td>
+                  <td className="py-2 px-4 border-b border-gray-700 text-center">{device.connected ? 'Conectado' : 'Desconectado'}</td>
+                  <td className="py-2 px-4 border-b border-gray-700 text-center">{device.isSos ? 'Activado' : 'Desactivado'}</td>
+                  <td className="py-2 px-4 border-b border-gray-700 text-center">{device.isWifi ? 'Sí' : 'No'}</td>
+                  <td className="py-2 px-4 border-b border-gray-700 text-center">{device.owner}</td>
+                  <td className="py-2 px-4 border-b border-gray-700 text-center">{device.contacts.join(', ')}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <ButtonPaged totalDevices={filteredDevices.length} devicesPerPage={devicesPerPage} />
+        </>
+      )}
     </div>
   );
 };
